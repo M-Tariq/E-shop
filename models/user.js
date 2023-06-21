5c15c42ee3feadc4425a87b925646dcc69b4993e
Tariq Muhammad <tariqmuhammad.appelit@nextbridge.com>
2023-06-21 23:19:30 +0500

    added users routes

--- models/user.js
+++ models/user.js
@@ -1,13 +1,47 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  passwordHash: String,
+  street: {
+    type: String,
+    default: "",
+  },
+  apartment: {
+    type: String,
+    default: "",
+  },
+  zip: {
+    type: String,
+    default: "",
+  },
+  country: {
+    type: String,
+    default: "",
+  },
 
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set("toJSON", {
+  virtual: true,
 });
 
 exports.User = mongoose.model("User", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,97 @@
-const { Product } = require('../models/product');
-const express = require('express');
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+const { User } = require("../models/user");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find();
+  try {
+    const users = await User.find().select("-passwordHash");
 
-  if (!productList) {
-    res.status(500).json({ success: false })
+    if (!users) {
+      res.status(500).json({ success: false });
+    }
+    res.status(200).json({
+      users,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
   }
-  res.status(200).json({
-    productList,
-    success: true
-  });
-})
+});
 
-router.post(`/`, (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock
-  })
+router.get(`/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (mongoose.isValidObjectId(id)) {
+      const users = await User.findById(id).select("-passwordHash");
 
-  product.save().then((createdProduct => {
-    res.status(201).json(createdProduct)
-  })).catch((err) => {
+      if (!users) {
+        return res.status(500).json({ success: false });
+      }
+      return res.status(200).json({
+        users,
+        success: true,
+      });
+    }
+    return res.status(500).json({ success: false, message: "Invalid Id." });
+  } catch (error) {
+    return res.status(500).json({ success: false, error });
+  }
+});
+
+router.put(`/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    let user = null;
+    if (mongoose.isValidObjectId(id)) {
+      user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+
+      if (!user) {
+        return res.status(500).json({ success: false });
+      }
+      return res.status(200).json({
+        user,
+        success: true,
+      });
+    }
+    return res.status(500).json({ success: false, message: "Invalid Id." });
+  } catch (error) {
+    return res.status(500).json({ success: false, error });
+  }
+});
+
+router.post(`/`, async (req, res) => {
+  try {
+    let salt = bcryptjs.genSaltSync(10);
+    let newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      passwordHash: bcryptjs.hashSync(req.body.password, salt),
+      street: req.body.street,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      country: req.body.country,
+      isAdmin: req.body.isAdmin,
+    });
+
+    newUser = await newUser.save();
+    if (!newUser) {
+      return res.status(500).json({
+        success: false,
+      });
+    }
+    return res.status(201).json({ user: newUser });
+  } catch (error) {
+    console.log(error);
     res.status(500).json({
-      error: err,
-      success: false
-    })
-  })
-})
+      error,
+      success: false,
+    });
+  }
+});
 
 module.exports = router;
